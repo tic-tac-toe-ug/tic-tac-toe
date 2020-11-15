8fb68a114b38222265d09ddb934f21e80aecb6c3
petlew <piotr@petlew.com>
2020-11-15 15:57:39 +0100

    Almost Initial Commit

---
+++ src/test/java/com/ug/grupa2/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ug.grupa2.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
