4122371ae3556b345f459faabe0b43ca79677aa1
petlew <piotr@petlew.com>
2020-11-15 16:38:41 +0100

    Adding angular integration

--- src/main/java/com/ug/grupa2/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/ug/grupa2/tictactoe/TicTacToeApplication.java
@@ -2,12 +2,30 @@ package com.ug.grupa2.tictactoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@Controller
 public class TicTacToeApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+
+	@GetMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
 	}
 
 }
