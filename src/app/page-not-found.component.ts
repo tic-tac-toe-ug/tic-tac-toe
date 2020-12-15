840466e7bf30a8a55b1772f9ead0ebbd9d421c19
petlew <piotr@petlew.com>
2020-12-15 05:45:07 +0100

    Added basic navigation

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -4,11 +4,4 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html'
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PageNotFoundComponent { }
