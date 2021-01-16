c4845f1b00cd2d1dba4894c837e3a240814cb60b
anna-malizjusz <a.malizjusz@vp.pl>
2021-01-16 17:07:12 +0100

    Remove leftovers

--- src/main/java/com/ug/grupa2/tictactoe/enums/MoveResult.java
+++ src/main/java/com/ug/grupa2/tictactoe/enums/MoveResult.java
@@ -27,18 +27,4 @@ public enum MoveResult {
         return new ResponseEntity<>("Correct move", HttpStatus.OK);
     }
   }
-
 }
-//
-//      case INVALID_MOVE:
-//        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//  case UNAUTHORIZED:
-//  return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//  case USER1_WON:
-//  return new ResponseEntity<>("\"data\": \"User1 won\"", HttpStatus.OK);
-//  case USER2_WON:
-//  return new ResponseEntity<>("\"data\": \"User2 won\"", HttpStatus.OK);
-//  case TIE:
-//  return new ResponseEntity<>("\"data\": \"Tie\"", HttpStatus.OK);
-//default:
-//  return new ResponseEntity<>("\"data\": \"Correct move\"", HttpStatus.OK);
