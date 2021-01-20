38b594085ab8e8ebc6b7c66dfa272f1390bb49d8
Ada Piekarska <40804491+adapiekarska@users.noreply.github.com>
2021-01-20 13:11:42 +0100

    Add unit tests for GameService (#14)
    * Add unit tests for GameService
    
    Co-authored-by: Ada <=>

--- src/main/java/com/ug/grupa2/tictactoe/entities/Game.java
+++ src/main/java/com/ug/grupa2/tictactoe/entities/Game.java
@@ -1,10 +1,7 @@
 package com.ug.grupa2.tictactoe.entities;
 
 import com.ug.grupa2.tictactoe.enums.GameStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -15,6 +12,7 @@ import java.sql.Timestamp;
 @lombok.Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Game {
 
   @Id
